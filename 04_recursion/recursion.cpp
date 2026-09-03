bea2221b79fe1e82a5a4619f4b870abe2a9a5822
BList0636 <blist0636@floridapoly.edu>
2026-09-03 14:53:21 -0400

    recursion

---
+++ 04_recursion/recursion.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+void countdown(int n){
+    if (n <= 0){
+        std::cout << "Reached base case\n";
+        return;
+    }
+    std::cout << n << " ";
+    countdown(n-1);
+}
+
+void countup(int n){
+    if (n <= 0){
+        std::cout << "Reached base case\n";
+        return;
+    }
+    countup(n-1);
+
+    std::cout << n << " ";
+}
+
+double power(double base, int exp){
+    if (exp < 0){
+        return power(base,exp-1)*base;
+    }
+    else if(exp > 0){
+        return power(base,exp+1)/base;
+    }
+    else{
+        return 1;
+    }
+}
+
+void print_binary(int num){
+    if (num <= 0){
+        
+    }
+}
+
+int main(){
+    countdown(10);
+    countup(10);
+
+    std::cout << power(2, 30);
+}
